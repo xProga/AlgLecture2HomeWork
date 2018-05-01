c69bcb6de6a64559e299320512c29edf62350634
Vadim Proga Krylov <progavs@gmail.com>
2018-05-01 19:45:45 +0500

    first commit

---
+++ Lecture2HomeWork/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture2HomeWork
+{
+    class Program
+    {
+        public class Tree
+        {
+            public class Node
+            {
+                public Node Right;
+                public Node Left;
+                public int Value;
+
+                public Node(int value)
+                {
+                    Right = null;
+                    Left = null;
+                    Value = value;
+                }
+            }
+
+            public Node Root;
+            public int Count = 0;
+
+            public Tree()
+            {
+                Root = null;
+            }
+
+            public Tree(int value)
+            {
+                Root = new Node(value);
+            }
+
+            public void AddNode()
+            {
+                _add(ref Root, Root.Value);
+            }
+
+            private void _add(ref Node node, int value)
+            {
+                if (value <= 20)
+                {
+                    node = new Node(value);
+                    if (node.Value * 2 < 21)
+                    {
+                        _add(ref node.Right, node.Value * 2);
+                    }
+                    if (node.Value + 1 < 21)
+                    {
+                        _add(ref node.Left, node.Value + 1);
+                    }
+                    if (node.Value == 20)
+                    {
+                        Count++;
+                    }
+                }
+            }
+
+            #region Mass
+            public void AddNodeMass(ref Node node, int value)
+            {
+                int i = 0;
+                while (true)
+                {
+                    if (value <= 20)
+                    {
+                        if (value + 1 < 21)
+                        {
+                            _addMass(ref node, ref node.Right, node.Value);
+                        }
+                        if (value * 2 < 21)
+                        {
+                            _addMass(ref node, ref node.Left, node.Value);
+                        }
+
+                    }
+                }
+            }
+
+            private void _addMass(ref Node prevNode, ref Node node, int value)
+            {
+                if (node == null)
+                {
+                    node = new Node(value);
+                    node.Prev = prevNode;
+                }
+            }
+
+            #endregion
+
+            public int DisplayCount()
+            {
+                return Count;
+            }
+        }
+
+        static bool Recur(int startIndex)
+        {
+            if (startIndex + 1 != 20)
+            {
+                if (startIndex * 2 == 20)
+                {
+                    return true;
+                }
+                return true;
+            }
+
+            return false;
+        }
+
+        static void Mass()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Рекурсия");
+            Tree tree = new Tree(3);
+            tree.AddNode();
+            Console.WriteLine($"Количество путей получения из числа 3 числа 20: {tree.DisplayCount()}");
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Lecture2HomeWorkTask3/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture2HomeWorkTask3
+{
+    class Program
+    {
+        public static int PopByCycle(int val, int pop)
+        {
+            if (pop == 0)
+            { return 1; }
+            int outputVal = 1;
+            while (pop > 0)
+            {
+                outputVal = outputVal * val;
+                pop--;
+            }
+            return outputVal;
+        }
+
+        public static void PopByRec(ref int val, int pop)
+        {
+
+            if (pop > 1)
+            {
+                int tempVal = val;
+                pop = pop - 1;
+                PopByRec(ref val, pop);
+                val = val * tempVal;
+            }
+            else
+            {
+                if (pop == 0) val = 1;
+            }
+
+        }
+
+        public static void PopByRec2(ref int val, int pop)
+        {
+            if (pop > 1)
+            {
+                if (pop % 2 != 0)
+                {
+                    int tempVal = val;
+                    pop--;
+                    PopByRec2(ref val, pop);
+                    val = val * tempVal;
+                }
+                else
+                {
+                    val = val * val;
+                    pop = pop / 2;
+                    PopByRec2(ref val, pop);
+                }
+            }
+            else {
+                if (pop == 0) val = 1;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            int origVal = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите степень:");
+            int pop = int.Parse(Console.ReadLine());
+            int val = origVal;
+            Console.WriteLine($"C помощью цикла: {PopByCycle(val, pop)}");
+            PopByRec(ref val, pop);
+            Console.WriteLine($"C помощью рекурсии: {val}");
+            val = origVal;
+            PopByRec2(ref val, pop);
+            Console.WriteLine($"C помощью цикла: {val}");
+            Console.ReadKey();
+        }
+    }
+}
